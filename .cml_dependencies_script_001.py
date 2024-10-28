7be54441fd1d603794380dd7ccfa14477ee96b18
Bruce Shimel <bshimel@cloudera.com>
2024-10-27 20:16:06 -0500

    first commit

---
+++ .cml_dependencies_script_001.py
@@ -0,0 +1,3 @@
+!pip3 install datasets "huggingface-hub>=0.19" "hf-transfer>=0.1.4" "protobuf<4" "click<8.1" "pydantic~=1.0"
+!pip3 install --no-cache-dir -r requirements.txt
+!pip3 install  gradio[oauth]==4.44.1  uvicorn>=0.14.0  spaces==0.16.1 fastapi==0.115.3
---
+++ README.md
@@ -0,0 +1,12 @@
+---
+title: Mistral Super Fast
+emoji: 😻
+colorFrom: red
+colorTo: yellow
+sdk: gradio
+sdk_version: 4.36.0
+app_file: app.py
+pinned: false
+---
+
+Check out the configuration reference at https://huggingface.co/docs/hub/spaces-config-reference
